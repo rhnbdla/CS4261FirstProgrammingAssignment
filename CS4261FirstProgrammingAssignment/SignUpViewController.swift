0b9275e8853f26c54f4703cc7a9f30a4cec7a9fa
Rohan Bodla <rbodla3@gatech.edu>
2023-09-12 01:39:46 -0400

    init commit

---
+++ CS4261FirstProgrammingAssignment/SignUpViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SignUpViewController.swift
+//  CS4261FirstProgrammingAssignment
+//
+//  Created by Rohan Bodla on 9/11/23.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        guard let email = usernameField.text else { return }
+        guard let password = passwordField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseCompletion, error in
+            if let e = error {
+                print("error: " + error.debugDescription)
+            } else {
+                //go to next
+                self.performSegue(withIdentifier: "successfulSignUp", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CS4261FirstProgrammingAssignment/WeatherPageViewController.swift
@@ -0,0 +1,111 @@
+//
+//  WeatherPageViewController.swift
+//  CS4261FirstProgrammingAssignment
+//
+//  Created by Rohan Bodla on 9/11/23.
+//
+
+import UIKit
+
+class WeatherPageViewController: UIViewController {
+
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var zipNumber: UITextField!
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyy-MM-dd hh:mm:ss"
+        dateLabel.text = dateFormatter.string(from: Date())
+        
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?zip=30313,us&units=imperial&appid=83e2558c233940e3acb9cbb2a9c5c98e") else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, error == nil {
+                do{
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else { return }
+                    guard let weatherDetails = json["weather"] as? [[String : Any]], let weatherMain = json["main"] as? [String : Any] else { return }
+                    let temp = Int(weatherMain["temp"] as? Double ?? 0)
+                    let description = (weatherDetails.first?["description"] as? String)?.capitalizingFirstLetter()
+                    let locationName = (json["name"] as? String ?? "...")?.capitalizingFirstLetter()
+                    let feelsLikeVal = Int(weatherMain["feels_like"] as? Double ?? 0)
+                    let maxTempVal = Int(weatherMain["temp_max"] as? Double ?? 0)
+                    let minTempVal = Int(weatherMain["temp_min"] as? Double ?? 0)
+                    let humidityVal = Int(weatherMain["humidity"] as? Double ?? 0)
+                    DispatchQueue.main.async {
+                        self.setWeather(weather: weatherDetails.first?["main"] as? String, description: description, temp: temp, locationName: locationName, feelsLikeVal: feelsLikeVal, maxTempVal: maxTempVal, minTempVal: minTempVal, humidityVal: humidityVal)
+                    }
+                }
+                catch{
+                    print("Error in retrieving weather")
+                }
+            }
+            
+        }
+        task.resume()
+    }
+    
+    @IBAction func weatherLookUp(_ sender: UIButton) {
+        if zipNumber.text?.count != 5 {
+            print("error in ZIP code")
+            return
+        }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?zip=\(zipNumber.text ?? "30313"),us&units=imperial&appid=83e2558c233940e3acb9cbb2a9c5c98e") else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, error == nil {
+                do{
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else { return }
+                    guard let weatherDetails = json["weather"] as? [[String : Any]], let weatherMain = json["main"] as? [String : Any] else { return }
+                    let temp = Int(weatherMain["temp"] as? Double ?? 0)
+                    let description = (weatherDetails.first?["description"] as? String)?.capitalizingFirstLetter()
+                    let locationName = (json["name"] as? String ?? "...")?.capitalizingFirstLetter()
+                    let feelsLikeVal = Int(weatherMain["feels_like"] as? Double ?? 0)
+                    let maxTempVal = Int(weatherMain["temp_max"] as? Double ?? 0)
+                    let minTempVal = Int(weatherMain["temp_min"] as? Double ?? 0)
+                    let humidityVal = Int(weatherMain["humidity"] as? Double ?? 0)
+                    DispatchQueue.main.async {
+                        self.setWeather(weather: weatherDetails.first?["main"] as? String, description: description, temp: temp, locationName: locationName, feelsLikeVal: feelsLikeVal, maxTempVal: maxTempVal, minTempVal: minTempVal, humidityVal: humidityVal)
+                    }
+//                    self.writeData(zipVal: self.zipNumber.text ?? "30313", temp: temp)
+                    
+                }
+                catch{
+                    print("Error in retrieving weather")
+                }
+            }
+            
+        }
+        task.resume()
+    }
+    func setWeather(weather: String?, description: String?, temp: Int, locationName: String?, feelsLikeVal: Int, maxTempVal: Int, minTempVal: Int, humidityVal: Int) {
+        descriptionLabel.text = description ?? "..."
+        temperatureLabel.text = "\(temp)°"
+        locationNameLabel.text = locationName ?? "..."
+        feelsLikeLabel.text = "Feels Like: \(feelsLikeVal)°"
+        maxTempLabel.text = "Max Temp: \(maxTempVal)°"
+        minTempLabel.text = "Min Temp: \(minTempVal)°"
+        humidityLabel.text = "Humidity: \(humidityVal)%"
+        switch weather {
+        case "Sunny":
+            background.backgroundColor = UIColor(red: 0.97, green: 0.78, blue: 0.35, alpha: 1.0)
+        default:
+            background.backgroundColor = UIColor(red: 0.42, green: 0.55, blue: 0.71, alpha: 1.0)
+        }
+        
+    }
+
+}
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+}
